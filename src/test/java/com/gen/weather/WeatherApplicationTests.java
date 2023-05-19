bd6ed22c616a2cd7c2cbc5b87c81da198132d670
Stephen Cunningham <stephencunningham@outlook.ie>
2023-05-19 02:33:33 +0100

    Initial commit

---
+++ src/test/java/com/gen/weather/WeatherApplicationTests.java
@@ -0,0 +1,11 @@
+package com.gen.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+	@Test
+	void contextLoads() {}
+}
